901a111599b7980326ebe6c01535e00c0c66501a
Gautam krishna.R <rgautamkrishna@gmail.com>
2015-11-07 19:24:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
-#include<fstream.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<fstream>
 using namespace std;
 void dis(int x){for(int i=0;i<=x;i++)cout<<"*";}
 int main()
 {
     system("Title Easy folder creator");
-    int j=1;
+    system("echo off");
     dis(78);
-    cout << "\n\t\t\t Easy folder creator" << endl;
+    cout << "\n\t\t Easy folder creator BY Gautam krishna.R" << endl;
     dis(78);
     int z;
     cout<<endl;
     fstream gout;
     cout<<"\n\t\tEtnter the number of folders you want to create : ";
     cin>>z;
+    gout.open("gen.bat",ios::out);
+    gout<<"echo off"<<endl<<"cls";
     for(int i=1;i<=z;i++)
     {
-        gout.open("gen.bat",ios::out);
-        gout<<"mkdir Folder_"<<i;
-        gout.close();
+        gout<<"echo       wait..."<<endl;
+        gout<<"mkdir Folder_"<<i<<endl<<"cls"<<endl;
+    }
+    gout.close();
         system("call gen.bat");
         system("del /F /s /Q gen.bat");
         system("cls");
-    }
         cout<<"\n\tSucess! "<<z<<"folders created";
     getchar();
+
 }
